18f9783ba49e95d083c7d0a29bca3254de59d88b
thienlhtst <89084577+thienlhtst@users.noreply.github.com>
2024-07-05 22:34:00 +0700

    sda

--- app/detailfood/page.tsx
+++ app/detailfood/page.tsx
@@ -5,6 +5,20 @@ import Image from "next/image";
 import React from "react";
 
 export default function Home() {
-   
-    return(<></>)
+
+    return (<>
+        <div className="flex flex-row w-full bg-white rounded-2xl">
+            <div className="w-1/2">
+                <Image src="/images/Ga.png" width={100} height={100} alt={""} />
+                
+            </div>
+            <div className="w-1/2">
+                    <div className="text-lg font-bold">Gà Rán</div>
+                    <div className="text-sm">Thức ăn nhanh</div>
+            </div>
+
+        </div>
+
+
+    </>)
 }
